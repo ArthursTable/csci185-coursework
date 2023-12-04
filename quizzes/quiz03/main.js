6c4d36e8c19cc03d198ac14e42a1b9d49b713876
Arthur <kbates2@unca.edu>
2023-12-04 16:00:10 -0500

    Quiz 3 (and some project stuff)

---
+++ quizzes/quiz03/main.js
@@ -0,0 +1,23 @@
+function color1() {
+    document.querySelector('#square1').style.backgroundColor = "#EEB4B3"
+}
+
+function color2() {
+    document.querySelector('#square2').style.backgroundColor = "#C179B9"
+}
+
+function color3() {
+    document.querySelector('#square3').style.backgroundColor = "#F4D06F"
+}
+
+function color4() {
+    document.querySelector('#square4').style.backgroundColor = "#502274"
+}
+
+function color5() {
+    document.querySelector('#square5').style.backgroundColor = "#9DD9D2"
+}
+
+function color6() {
+    document.querySelector('#square6').style.backgroundColor = "#C7EFCF"
+}
